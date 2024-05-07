f66fa649ee6f561e7f0e7cba9a3d41725a65af6d
MiguelBits <mcv.cooper@gmail.com>
2024-05-07 18:44:20 +0100

    pushed

---
+++ README.md
@@ -0,0 +1,8 @@
+https://api.studio.thegraph.com/query/64123/nft-sentience/version/latest
+
+npm install -g @graphprotocol/graph-cli
+graph init --studio nft-sentience
+graph auth --studio $APIKEY
+cd nft-sentience
+graph codegen && graph build
+graph deploy --studio nft-sentience
---
+++ src/helper.ts
@@ -0,0 +1,67 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import {HolderSentienceOne, HolderSentienceTwo, HolderSentienceThree, HolderSentienceFour, HolderSentienceFive} from "../generated/schema"
+
+export function loadOrCreateHolderOne (address: Address): HolderSentienceOne{
+    let holderEntity = HolderSentienceOne.load(address)
+
+    if (!holderEntity){
+        holderEntity = new HolderSentienceOne(address)
+        holderEntity.holdCount = BigInt.fromI32(0);
+        let newIds = new Array<BigInt>(0);
+        holderEntity.holdingIds = newIds;
+    }
+    
+    return holderEntity as HolderSentienceOne;
+}
+
+export function loadOrCreateHolderTwo (address: Address): HolderSentienceTwo{
+    let holderEntity = HolderSentienceTwo.load(address)
+
+    if (!holderEntity){
+        holderEntity = new HolderSentienceTwo(address)
+        holderEntity.holdCount = BigInt.fromI32(0);
+        let newIds = new Array<BigInt>(0);
+        holderEntity.holdingIds = newIds;
+    }
+    
+    return holderEntity as HolderSentienceTwo;
+}
+
+export function loadOrCreateHolderThree (address: Address): HolderSentienceThree{
+    let holderEntity = HolderSentienceThree.load(address)
+
+    if (!holderEntity){
+        holderEntity = new HolderSentienceThree(address)
+        holderEntity.holdCount = BigInt.fromI32(0);
+        let newIds = new Array<BigInt>(0);
+        holderEntity.holdingIds = newIds;
+    }
+    
+    return holderEntity as HolderSentienceThree;
+}
+
+export function loadOrCreateHolderFour (address: Address): HolderSentienceFour{
+    let holderEntity = HolderSentienceFour.load(address)
+
+    if (!holderEntity){
+        holderEntity = new HolderSentienceFour(address)
+        holderEntity.holdCount = BigInt.fromI32(0);
+        let newIds = new Array<BigInt>(0);
+        holderEntity.holdingIds = newIds;
+    }
+    
+    return holderEntity as HolderSentienceFour;
+}
+
+export function loadOrCreateHolderFive (address: Address): HolderSentienceFive{
+    let holderEntity = HolderSentienceFive.load(address)
+
+    if (!holderEntity){
+        holderEntity = new HolderSentienceFive(address)
+        holderEntity.holdCount = BigInt.fromI32(0);
+        let newIds = new Array<BigInt>(0);
+        holderEntity.holdingIds = newIds;
+    }
+    
+    return holderEntity as HolderSentienceFive;
+}
