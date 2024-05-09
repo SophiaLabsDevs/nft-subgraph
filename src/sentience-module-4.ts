6cee0cf26aa2576d5c64ae3cfcd56b75af52d82c
c0rdeiro <francisco.cordeiro.business@gmail.com>
2024-05-09 17:02:09 +0100

    fix edge case where sender == receiver

--- src/sentience-module-4.ts
+++ src/sentience-module-4.ts
@@ -1,46 +1,45 @@
-import {
-  Transfer as TransferEvent,
-} from "../generated/SentienceModule4/SentienceModule4"
-import { SentienceModule4Transfer as Transfer } from "../generated/schema"
-import { loadOrCreateHolderFour } from "./helper"
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Transfer as TransferEvent } from "../generated/SentienceModule4/SentienceModule4";
+import { SentienceModule4Transfer as Transfer } from "../generated/schema";
+import { loadOrCreateHolderFour } from "./helper";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handleTransferFour(event: TransferEvent): void {
   let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
+  if (event.params.from === event.params.to) return; //end here id sender is the same as recipient
 
   let recipient = loadOrCreateHolderFour(event.params.to);
   let sender = loadOrCreateHolderFour(event.params.from);
 
   recipient.holdCount = recipient.holdCount.plus(BigInt.fromI32(1));
 
-  if(sender.holdCount.gt(BigInt.fromI32(0))){
+  if (sender.holdCount.gt(BigInt.fromI32(0))) {
     sender.holdCount = sender.holdCount.minus(BigInt.fromI32(1));
-  }
-  else{
+  } else {
     sender.holdCount = BigInt.fromI32(0);
   }
 
   //SENDER
-  if(sender.holdingIds.length > 1){
+  if (sender.holdingIds.length > 1) {
     //search for the token id in the array and remove it
     let neWIds = sender.holdingIds;
     let index = neWIds.indexOf(event.params.tokenId);
     //if the token id is in the array, then remove it
-    if(index > -1){
+    if (index > -1) {
       neWIds.splice(index, 1);
       sender.holdingIds = neWIds;
     }
-  }
-  else{
+  } else {
     //if the array has only one element, then it is the token id and we can just reset the array
     let newSenderIds = new Array<BigInt>(0);
     sender.holdingIds = newSenderIds;
@@ -50,8 +49,6 @@ export function handleTransferFour(event: TransferEvent): void {
   let newIds = recipient.holdingIds;
   newIds.push(event.params.tokenId);
   recipient.holdingIds = newIds;
-
-  entity.save();
 
   sender.save();
   recipient.save();
